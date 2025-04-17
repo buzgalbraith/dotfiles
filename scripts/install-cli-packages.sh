3bd768c204576f95fa51a69d32f2e6505a1b21be
buz galbraith <buzgalbraith@gmail.com>
2025-04-17 16:36:54 -0400

    added mouse copying for tmux and plugin manager

--- scripts/install-cli-packages.sh
+++ scripts/install-cli-packages.sh
@@ -5,3 +5,5 @@ sudo apt-get update
 sudo apt-get install -y \
     vim tmux git \
     gh 
+## install tmux package manager as well
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

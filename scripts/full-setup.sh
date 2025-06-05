2b7340c0b000f4bbddce0e2f1625634a7b830efa
buz galbraith <buzgalbraith@gmail.com>
2025-06-05 17:21:23 -0400

    removed scripts not used on remote systems

--- scripts/full-setup.sh
+++ scripts/full-setup.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 set -e
 
-## install command line packages 
-bash scripts/install-cli-packages.sh
-
-## setup python environment 
-bash scripts/python-setup.sh
 
 ## setup symlinks 
 bash scripts/make-symlinks.sh
 
-## remap keys 
-bash scripts/remap.sh
-
-## install gui packages
-bash scripts/install-gui-packages.sh
-
-
+## setup vim
+bash scripts/vim-setup.py
